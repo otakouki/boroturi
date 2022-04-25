089dd9dd5ad8b6530b40a0762cd961c036096bfa
otakouki <koki.oot@gmail.com>
2022-04-26 00:58:45 +0900

    docker

--- boroturi/www/html/index.php
+++ boroturi/www/html/index.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo();
+
+define('HOST','boroturi-db-1');
+define('USR','ecc');
+define('PASS','ecc');
+define('DB','boroturi');
+
+if (!$con = mysqli_connect(HOST, USR, PASS, DB)) {
+        //データベースに接続できないときのメッセージ
+        die('データベースに接続できません');
+}
 ?>
